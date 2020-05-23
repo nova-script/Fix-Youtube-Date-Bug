4e3208c3e069befbd7bcde3c7269813c4fdad1b6
José Guilherme Fernandes Moura <joseguilhermefmoura@gmail.com>
2020-05-23 12:33:25 -0300

    Update script.js

--- Fix-Youtube-Date-Bug/script.js
+++ Fix-Youtube-Date-Bug/script.js
@@ -1,5 +1,5 @@
-window.onload = function() {
-    $('#date').remove().insertAfter($("#owner-sub-count"));
-    $('#dot').remove();
-    $("#date").css({"font-size": "1.3rem"});
-};
+window.addEventListener('load', 
+  function() { 
+        $("#date").remove().insertAfter($("#owner-sub-count"));
+    	$("#date").find("span").remove();
+  }, false);
